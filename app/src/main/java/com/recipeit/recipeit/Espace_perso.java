77c0e30ed7d3af8efe9245d9d94e82b74b0e8d76
neluth <elenaa.t@hotmail.fr>
2018-03-01 19:01:57 +0100

    espace perso

--- app/src/main/java/com/recipeit/recipeit/Espace_perso.java
+++ app/src/main/java/com/recipeit/recipeit/Espace_perso.java
@@ -2,6 +2,10 @@ package com.recipeit.recipeit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Espace_perso extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class Espace_perso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_espace_perso);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        TextView txt = findViewById(R.id.titreEspaceP2);
+        txt.append(" " + user.getEmail());
     }
 }
